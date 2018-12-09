0b169673a5cf298f032a37bde9fa9794a75405c7
David Hernquist <david_hernquist@gmail.com>
2018-12-08 21:53:55 -0500

    reducers and actions finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {fetchTasks} from "./services/tasks";
+import {
+  fetchTasks
+} from "./services/tasks";
 import axios from "axios";
+import
 
 
 class App extends Component {
-  componentDidMount() {
-    // axios.get('http://localhost:3000/tasks')
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
-    fetchTasks.then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
 
-  }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      Tasks / >
+      <
+      /div>
     );
   }
 }
---
+++ src/store/reducer.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_TASKS_REQUEST,
+  FETCH_TASKS_SUCCESS,
+  FETCH_TASKS_FAILURE
+} from "../constants";
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  const {
+    type,
+    tasks,
+    err
+  } = action;
+  switch (type) {
+    case FETCH_TASKS_REQUEST:
+      return { ...state,
+        error: null,
+        loading: true
+      }
+    case FETCH_TASKS_SUCCESS:
+      return { ...state,
+        loading: false,
+        data: tasks
+      }
+    case FETCH_TASKS_FAILURE:
+      return { ...state,
+        loading: false,
+        error: err
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
