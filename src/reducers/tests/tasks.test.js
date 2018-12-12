9f9fc9ee1c75e5fe29d311eab894b250480ef972
David Hernquist <david_hernquist@gmail.com>
2018-12-11 21:06:42 -0500

    reducers tested

--- src/reducers/tests/tasks.test.js
+++ src/reducers/tests/tasks.test.js
@@ -20,36 +20,49 @@ const testState = {
 
 describe('src/reducers/tasks', () => {
   describe('FETCH_TASKS_REQUEST', () => {
+    const action = { type: FETCH_TASKS_REQUEST }
 
     it('returns state with a loading set to true and error set to null', () => {
-      const action = { type: FETCH_TASKS_REQUEST }
       const state = reducer(initialState, action);
       expect(state.loading).toBe(true);
       expect(state.error).toBe(null)
     })
     
     it('resets data to an empty array', () => {
-      const action = {
-        type: FETCH_TASKS_SUCCESS, 
-        tasks
-      }
+      expect(testState.data).toHaveLength(4);
       const state = reducer(initialState, action)
+      expect(state.data).toHaveLength(0);
     })
   })
   
   describe('FETCH_TASKS_SUCCESS', () => {
+    const action = ({ type : FETCH_TASKS_SUCCESS, tasks })
+    const state = reducer(initialState, action);
+
     it('returns state with a new list of tasks ', () => {
+      expect(state.data).toHaveLength(4);
+      expect(state.data[3].title).toBe("safety check");
     })
 
     it('set loading to false and keeps error unchanged', () => {
+      expect(state.loading).toBe(false);
+      expect(state.error).toBe(null);
     })
   })
   
   describe('FETCH_TASKS_FAILURE', () => {
+    const action = ({ 
+      type : FETCH_TASKS_FAILURE, 
+      err: { message: "bad stuff"}
+    })
+    const state = reducer(initialState, action);
+
     it('returns an error message to state', () => {
+      expect(state.error.message).toBe("bad stuff")
     })
     
     it('sets loading to true', () => {
+      expect(state.loading).toBe(false);
     })
   })
 })
